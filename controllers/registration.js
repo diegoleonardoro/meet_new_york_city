79a018f5f88e579617a8e296da3aa08833716e9f
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2022-01-11 19:35:27 -0500

    new nodemailer email

--- controllers/registration.js
+++ controllers/registration.js
@@ -157,7 +157,7 @@ const sendEmailConfirmation = async (user) => {
     const transport = nodemailer.createTransport({
         //host: process.env.NODEMAILER_HOST,
         //port: process.env.NODEMAILER_PORT,
-        service: 'Gmail',
+        service: 'smtp-mail.outlook.com',
         auth: {
             user: process.env.NODE_MAILER_GMAIL_USER,//process.env.NODEMAILER_USER,
             pass: process.env.NODE_MAILER_GMAIL_PASSWORD //process.env.NODEMAILER_PASSWORD
